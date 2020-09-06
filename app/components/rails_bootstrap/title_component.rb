4d1fade63a04845b13c2d33e1679bb3dfbdf75f0
Aman Patel <patel1wq@uwindsor.ca>
2020-09-05 20:34:07 -0400

    extend all components from  application_component.rb instead of ViewComponent::Base

--- app/components/rails_bootstrap/title_component.rb
+++ app/components/rails_bootstrap/title_component.rb
@@ -1,5 +1,6 @@
-class RailsBootstrap::TitleComponent < ViewComponent::Base
-  include ActiveModel::Validations
+# frozen_string_literal: true
+
+class RailsBootstrap::TitleComponent < ApplicationComponent
   
   TYPES = ["h1", "h2", "h3", "h4", "h5", "p"].freeze
 
