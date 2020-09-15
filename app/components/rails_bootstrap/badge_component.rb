d0243fadd2db3efb9461ef296f86e388e3f3b3a7
Prakort <leanprakort@gmail.com>
2020-09-15 11:24:20 -0400

    rename variantion to variation

--- app/components/rails_bootstrap/badge_component.rb
+++ app/components/rails_bootstrap/badge_component.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
 class RailsBootstrap::BadgeComponent < ApplicationComponent
-  
-  VARIRANTIONS = ["primary", "secondary", "success", "danger", "warning", "light", "info", "dark"].freeze
+  # rename variant to variation 
+  # variant is a keyword in rails
+  VARIATIONS = ["primary", "secondary", "success", "danger", "warning", "light", "info", "dark"].freeze
 
-  validates :variantion, inclusion: { in: VARIRANTIONS }
+  validates :variation, inclusion: { in: VARIATIONS }
   validates :pill, inclusion: { in: [true, false] }
 
-  def initialize(variantion: "primary", pill: false)
-    @variantion = "badge-#{variantion}"
+  def initialize(variation: "primary", pill: false)
+    @variation = "badge-#{variation}"
     @pill = pill ? "badge-pill" : ""
   end
-  
+
   private 
     
-    attr_reader :variantion, :pill
+    attr_reader :variation, :pill
 end
