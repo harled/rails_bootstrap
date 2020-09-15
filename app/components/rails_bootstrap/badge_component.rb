685e7357f24a78c16e7da7ce8b2eb838f2e0ac58
Prakort <leanprakort@gmail.com>
2020-09-15 11:18:18 -0400

    add class to badge

--- app/components/rails_bootstrap/badge_component.rb
+++ app/components/rails_bootstrap/badge_component.rb
@@ -2,16 +2,17 @@
 
 class RailsBootstrap::BadgeComponent < ApplicationComponent
   
-  VARIRANTS = ["primary", "secondary", "success", "danger", "warning", "light", "info", "dark"].freeze
+  VARIRANTIONS = ["primary", "secondary", "success", "danger", "warning", "light", "info", "dark"].freeze
 
-  validates :variant, inclusion: { in: VARIRANTS }
+  validates :variantion, inclusion: { in: VARIRANTIONS }
   validates :pill, inclusion: { in: [true, false] }
 
-  def initialize(variant: "primary", pill: false)
-    @variant = variant
-    @pill = pill 
+  def initialize(variantion: "primary", pill: false)
+    @variantion = "badge-#{variantion}"
+    @pill = pill ? "badge-pill" : ""
   end
+  
   private 
     
-    attr_reader :variant, :pill
+    attr_reader :variantion, :pill
 end
