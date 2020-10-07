f9ac9bc0cea7d01cbad04e29b20ceca85aa9b648
Prakort <leanprakort@gmail.com>
2020-10-07 15:48:19 -0400

    request changes made

--- spec/components/rails_bootstrap/badge_component_spec.rb
+++ spec/components/rails_bootstrap/badge_component_spec.rb
@@ -1,23 +1,17 @@
 require "rails_helper"
 
 RSpec.describe RailsBootstrap::BadgeComponent, type: :component do 
-  context "variation attribute tests" do
+  context "renders badge" do
     it "renders default variation" do 
-      subject = render_inline(RailsBootstrap::BadgeComponent.new()) { "This is a default badge" }
+      subject = render_inline(RailsBootstrap::BadgeComponent.new) { "This is a default badge" }
 
       expect(subject.css("span[class='badge badge-primary']").to_html).not_to be_empty
     end
 
-    it "renders secondary style" do 
+    it "sets variation to secondary" do 
       subject = render_inline(RailsBootstrap::BadgeComponent.new(variation: "secondary")) { "This is the secondary style badge" }
 
       expect(subject.css("span[class='badge badge-secondary']").to_html).not_to be_empty
-    end
-
-    it "renders info style" do 
-      subject = render_inline(RailsBootstrap::BadgeComponent.new(variation: "info")) { "This is the info style badge" }
-
-      expect(subject.css("span[class='badge badge-info']").to_html).not_to be_empty
     end
 
     it "renders the content" do 
@@ -25,9 +19,7 @@ RSpec.describe RailsBootstrap::BadgeComponent, type: :component do
 
       expect(subject.css("span[class='badge badge-success']").to_html).to include("This is the success style badge")
     end
-  end 
 
-  context "pill attribute tests" do
     it "renders badge pill" do 
       subject = render_inline(RailsBootstrap::BadgeComponent.new(pill: true)) { "This is a pill badge" }
 
